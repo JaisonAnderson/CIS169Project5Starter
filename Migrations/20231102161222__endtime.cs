31ade0ea056abbf08917dae19322b9b5fdbac8a1
Jaiso <Jaisonanderson04@gmail.com>
2023-11-02 11:21:47 -0500

    Adding field, updating model, adding seeding script, fixing links

---
+++ Migrations/20231102161222__endtime.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace CourseCatalog.Migrations
+{
+    /// <inheritdoc />
+    public partial class _endtime : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<TimeOnly>(
+                name: "EndTIme",
+                table: "Course",
+                type: "TEXT",
+                nullable: false,
+                defaultValue: new TimeOnly(0, 0, 0));
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "EndTIme",
+                table: "Course");
+        }
+    }
+}
